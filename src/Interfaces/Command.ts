f02687e73d189c413432498cb322da342d58be4d
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2022-03-28 20:04:19 -0700

    added a command interface

---
+++ src/Interfaces/Command.ts
@@ -0,0 +1,13 @@
+import { Message, PermissionResolvable } from "discord.js";
+import { Quabos } from "../Quabos";
+
+export interface Command {
+  name: string;
+  aliases: string[];
+  description: string;
+  usage: string;
+  cooldown: number;
+  userPermission: PermissionResolvable[];
+  botPermission: PermissionResolvable[];
+  execute(client: Quabos, message: Message, args: string[]): Promise<void>;
+}
--- src/Quabos.ts
+++ src/Quabos.ts
@@ -1,11 +1,21 @@
-import { Client } from 'discord.js';
+import {
+  Client,
+  Collection,
+  Message,
+  MessageEmbed,
+  MessageEmbedOptions,
+} from 'discord.js';
 import { Config } from './Interfaces/Config';
+import { Command } from './Interfaces/Command';
+import path from 'path';
+import fs from 'fs';
 
 export class Quabos extends Client {
   public name: string;
   public owners: string[];
   public prefix: string;
   public config: Config;
+  public commands: Collection<string, Command> = new Collection();
 
   constructor(config: Config) {
     super({
@@ -23,9 +33,51 @@ export class Quabos extends Client {
     this.once('ready', () => {
       console.log(`${this.name} is ready. Logged in as ${this.user?.tag}`);
     });
+
+    this.on('messageCreate', (message: Message) => {
+      if (!message.guild) return;
+      if (!message.guild.available) return;
+      if (message.author.bot) return;
+
+      if (!message.content.startsWith(this.prefix)) return;
+
+      const args = message.content.slice(this.prefix.length).split(/ +/);
+      const target = args.shift()?.toLowerCase();
+
+      const command = this.commands.find((cmd) => cmd.name === target);
+      if (!command) return;
+
+      const clientPermission = message.member?.permissions.has(
+        command.userPermission,
+        true
+      );
+      if (!clientPermission) return;
+
+      const botPermission = message.guild.me?.permissions.has(
+        command.botPermission,
+        true
+      );
+      if (!botPermission) return;
+
+      command.execute(this, message, args);
+    });
   }
 
   public async init() {
     await this.login(this.config.token);
+
+    const commandDir = path.join(__dirname, 'Commands');
+
+    fs.readdirSync(commandDir)
+      .filter((file) => file.endsWith('.js'))
+      .forEach(async (file) => {
+        const { default: defaultExport } = await import(`${commandDir}/${file}`);
+        const cmd = defaultExport as Command;
+        this.commands.set(cmd.name, cmd);
+      });
+  }
+
+  public embed(options: MessageEmbedOptions): MessageEmbed {
+    return new MessageEmbed({ ...options });
   }
 }
