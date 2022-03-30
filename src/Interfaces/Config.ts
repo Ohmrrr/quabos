f93ef8446e2458431927e1aabb074a71bb91c99f
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2022-03-29 22:28:23 -0700

    adding more config for markov and presence

--- src/Interfaces/Config.ts
+++ src/Interfaces/Config.ts
@@ -1,6 +1,22 @@
+type Type = 'PLAYING' | 'LISTENING' | 'WATCHING' | 'STREAMING' | 'COMPETING';
+type StateSize = 1 | 2 | 3;
+
 export interface Config {
   name: string;
   owners: string[];
   prefix: string;
   token: string;
+  markov: Markov;
+  presence: Presence;
+}
+
+interface Presence {
+  status: string;
+  activity: string;
+  type: Type;
+}
+
+interface Markov {
+  stateSize: StateSize;
+  maxTries: number;
 }
